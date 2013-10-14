887690b4c64cf32e9be2c19d927257e5f661ae08
James Saryerwinnie <js@jamesls.com>
2013-10-14 12:38:21 -0700

    Improve lexer/parser error messages
    Lexer error messages:
    
    jmespath.compile('fooasdfasdfasdfasdf.\"bar')"
    jmespath.lexer.LexerError: Bad jmespath expression: Bad token '"bar': starting quote is missing the ending quote:
    fooasdfasdfasdfasdf."bar
                        ^
    
    Parser error messages:
    
    jmespath.compile('foo]baz')"
    jmespath.parser.ParseError: Invalid jmespath expression: Parse error at column 3 near token "]" (RBRACKET) for expression:
    "foo]baz"
        ^

--- jmespath/parser.py
+++ jmespath/parser.py
@@ -7,6 +7,41 @@ from jmespath import ast
 from jmespath import lexer
 
 
+class ParseError(ValueError):
+    def __init__(self, lex_position, token_value, token_type):
+        super(ParseError, self).__init__(lex_position, token_value, token_type)
+        self.lex_position = lex_position
+        self.token_value = token_value
+        self.token_type = token_type
+        # Whatever catches the ParseError can fill in the full expression
+        self.expression = None
+
+    def __str__(self):
+        # self.lex_position +1 to account for the starting double quote char.
+        underline = ' ' * (self.lex_position + 1) + '^'
+        return (
+            'Invalid jmespath expression: Parse error at column %s near '
+            'token "%s" (%s) for expression:\n"%s"\n%s' % (
+                self.lex_position, self.token_value, self.token_type,
+                self.expression, underline))
+
+
+class IncompleteExpressionError(ParseError):
+    def set_expression(self, expression):
+        self.expression = expression
+        self.lex_position = len(expression)
+        self.token_type = None
+        self.token_value = None
+
+    def __str__(self):
+        # self.lex_position +1 to account for the starting double quote char.
+        underline = ' ' * (self.lex_position + 1) + '^'
+        return (
+            'Invalid jmespath expression: Incomplete expression:\n'
+            '"%s"\n%s' % (self.expression, underline))
+
+
+
 class Grammar(object):
     precedence = (
         ('right', 'DOT', 'LBRACKET'),
@@ -104,9 +139,10 @@ class Grammar(object):
         p[0] = ast.ORExpression(p[1], p[3])
 
     def p_error(self, t):
-        raise ValueError(
-            'Parse error at column %s near token %s (%s)' % (
-                t.lexpos, t.value, t.type))
+        if t is not None:
+            raise ParseError(t.lexpos, t.value, t.type)
+        else:
+            raise IncompleteExpressionError(None, None, None)
 
 
 class Parser(object):
@@ -137,11 +173,26 @@ class Parser(object):
         grammar.tokens = self._lexer_definition.tokens
         parser = ply.yacc.yacc(module=grammar, debug=self._debug,
                                write_tables=False)
-        parsed = parser.parse(input=expression, lexer=lexer)
+        parsed = self._parse_expression(parser=parser, expression=expression,
+                                        lexer_obj=lexer)
         self._cache[expression] = parsed
         if len(self._cache) > self._max_size:
             self._free_cache_entries()
         return parsed
+
+    def _parse_expression(self, parser, expression, lexer_obj):
+        try:
+            parsed = parser.parse(input=expression, lexer=lexer_obj)
+            return parsed
+        except lexer.LexerError as e:
+            e.expression = expression
+            raise e
+        except IncompleteExpressionError as e:
+            e.set_expression(expression)
+            raise e
+        except ParseError as e:
+            e.expression = expression
+            raise e
 
     def _free_cache_entries(self):
         # This logic is borrowed from the new regex library which
