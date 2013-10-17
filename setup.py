4ebc894d0ce546f7cbf48c7ea1ab7abba2bb6774
James Saryerwinnie <js@jamesls.com>
2013-10-17 15:38:46 -0700

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmespath',
-    version='0.0.3',
+    version='0.1.0',
     description='JSON Matching Expressions',
     long_description=open('README.rst').read(),
     author='James Saryerwinnie',
