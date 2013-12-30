2ecf1108d2dcaf8ed143c00522d93fa412312b5b
James Saryerwinnie <js@jamesls.com>
2013-12-30 12:44:29 -0800

    Add support for syntax error tests

--- tests/test_compliance.py
+++ tests/test_compliance.py
@@ -16,9 +16,13 @@ TEST_DIR = os.path.join(
 def test_compliance():
     for full_path in _walk_files():
         if full_path.endswith('.json'):
-            for given, expression, result in _load_cases(full_path):
-                yield (_test_expression, given, expression,
-                       result, os.path.basename(full_path))
+            for given, expression, result, error in _load_cases(full_path):
+                if error is None and result is not None:
+                    yield (_test_expression, given, expression,
+                        result, os.path.basename(full_path))
+                elif result is None and error is not None:
+                    yield (_test_error_expression, given, expression,
+                           error, os.path.basename(full_path))
 
 
 def _walk_files():
@@ -39,7 +43,8 @@ def _load_cases(full_path):
     for test_data in all_test_data:
         given = test_data['given']
         for case in test_data['cases']:
-            yield given, case['expression'], case['result']
+            yield (given, case['expression'],
+                   case.get('result'), case.get('error'))
 
 
 def _test_expression(given, expression, expected, filename):
@@ -52,7 +57,24 @@ def _test_expression(given, expression, expected, filename):
     actual = parsed.search(given)
     expected_repr = json.dumps(expected, indent=4)
     actual_repr = json.dumps(actual, indent=4)
-    error_msg = ("\n(%s) The expression '%s' was suppose to give: %s.\n"
+    error_msg = ("\n\n  (%s) The expression '%s' was suppose to give: %s.\n"
                  "Instead it matched: %s\nparsed as:\n%s" % (
                      filename, expression, expected_repr, actual_repr, parsed))
+    error_msg = error_msg.replace(r'\n', '\n')
     assert_equal(actual, expected, error_msg)
+
+
+def _test_error_expression(given, expression, error, filename):
+    if not error == 'syntax':
+        raise RuntimeError("Unknown error type '%s'" % error)
+    try:
+        parsed = jmespath.compile(expression)
+    except ValueError as e:
+        # Test passes, it raised a parse error as expected.
+        pass
+    else:
+        error_msg = ("\n\n  (%s) The expression '%s' was suppose to be a "
+                     "syntax error, but it successfully parsed as:\n\n%s" % (
+                         filename, expression, parsed))
+        error_msg = error_msg.replace(r'\n', '\n')
+        raise AssertionError(error_msg)
