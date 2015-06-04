a234f7367afdeb906b2d16a89516d5f0e0150f8f
Michael Dowling <mtdowling@gmail.com>
2015-06-03 17:01:44 -0700

    Using a stateful lexer rather than a Scanner object

--- jmespath/lexer.py
+++ jmespath/lexer.py
@@ -5,43 +5,6 @@ from json import loads
 from jmespath.exceptions import LexerError, EmptyExpressionError
 
 
-class Scanner(object):
-    def __init__(self, expression):
-        if not expression:
-            raise EmptyExpressionError()
-        self.expression = expression
-        self.pos = 0
-        self.chars = list(self.expression)
-        self.len = len(self.expression)
-        self.current = self.chars[self.pos]
-
-    def next(self):
-        if self.pos == self.len - 1:
-            self.current = None
-        else:
-            self.pos += 1
-            self.current = self.chars[self.pos]
-        return self.current
-
-    def in_delimiter(self, delimiter):
-        start = self.pos
-        buff = ''
-        self.next()
-        while self.current != delimiter:
-            if self.current == '\\':
-                buff += '\\'
-                self.next()
-            if self.current is None:
-                raise LexerError(lexer_position=start,
-                                 lexer_value=self.expression,
-                                 message="Unclosed %s delimiter" % delimiter)
-            buff += self.current
-            self.next()
-        # Skip the closing delimiter.
-        self.next()
-        return buff
-
-
 class Lexer(object):
     START_IDENTIFIER = set(string.ascii_letters + '_')
     VALID_IDENTIFIER = set(string.ascii_letters + string.digits + '_')
@@ -63,69 +26,104 @@ class Lexer(object):
     }
 
     def tokenize(self, expression):
-        scanner = Scanner(expression)
-        while scanner.current is not None:
-            if scanner.current in self.SIMPLE_TOKENS:
-                yield {'type': self.SIMPLE_TOKENS[scanner.current],
-                       'value': scanner.current,
-                       'start': scanner.pos, 'end': scanner.pos + 1}
-                scanner.next()
-            elif scanner.current in self.START_IDENTIFIER:
-                start = scanner.pos
-                buff = scanner.current
-                while scanner.next() in self.VALID_IDENTIFIER:
-                    buff += scanner.current
+        self._init_expr(expression)
+        while self._current is not None:
+            if self._current in self.SIMPLE_TOKENS:
+                yield {'type': self.SIMPLE_TOKENS[self._current],
+                       'value': self._current,
+                       'start': self._pos, 'end': self._pos + 1}
+                self._next()
+            elif self._current in self.START_IDENTIFIER:
+                start = self._pos
+                buff = self._current
+                while self._next() in self.VALID_IDENTIFIER:
+                    buff += self._current
                 yield {'type': 'unquoted_identifier', 'value': buff,
                        'start': start, 'end': start + len(buff)}
-            elif scanner.current in self.WHITESPACE:
-                scanner.next()
-            elif scanner.current == '[':
-                start = scanner.pos
-                next_char = scanner.next()
+            elif self._current in self.WHITESPACE:
+                self._next()
+            elif self._current == '[':
+                start = self._pos
+                next_char = self._next()
                 if next_char == ']':
-                    scanner.next()
+                    self._next()
                     yield {'type': 'flatten', 'value': '[]',
                            'start': start, 'end': start + 2}
                 elif next_char == '?':
-                    scanner.next()
+                    self._next()
                     yield {'type': 'filter', 'value': '[?',
                            'start': start, 'end': start + 2}
                 else:
                     yield {'type': 'lbracket', 'value': '[',
                            'start': start, 'end': start + 1}
-            elif scanner.current == "'":
-                yield self._consume_raw_string_literal(scanner)
-            elif scanner.current == '|':
-                yield self._match_or_else(scanner, '|', 'or', 'pipe')
-            elif scanner.current == '`':
-                yield self._consume_literal(scanner)
-            elif scanner.current in self.START_NUMBER:
-                start = scanner.pos
-                buff = scanner.current
-                while scanner.next() in self.VALID_NUMBER:
-                    buff += scanner.current
+            elif self._current == "'":
+                yield self._consume_raw_string_literal()
+            elif self._current == '|':
+                yield self._match_or_else('|', 'or', 'pipe')
+            elif self._current == '`':
+                yield self._consume_literal()
+            elif self._current in self.START_NUMBER:
+                start = self._pos
+                buff = self._current
+                while self._next() in self.VALID_NUMBER:
+                    buff += self._current
                 yield {'type': 'number', 'value': int(buff),
                        'start': start, 'end': start + len(buff)}
-            elif scanner.current == '"':
-                yield self._consume_quoted_identifier(scanner)
-            elif scanner.current == '<':
-                yield self._match_or_else(scanner, '=', 'lte', 'lt')
-            elif scanner.current == '>':
-                yield self._match_or_else(scanner, '=', 'gte', 'gt')
-            elif scanner.current == '!':
-                yield self._match_or_else(scanner, '=', 'ne', 'unknown')
-            elif scanner.current == '=':
-                yield self._match_or_else(scanner, '=', 'eq', 'unknown')
+            elif self._current == '"':
+                yield self._consume_quoted_identifier()
+            elif self._current == '<':
+                yield self._match_or_else('=', 'lte', 'lt')
+            elif self._current == '>':
+                yield self._match_or_else('=', 'gte', 'gt')
+            elif self._current == '!':
+                yield self._match_or_else('=', 'ne', 'unknown')
+            elif self._current == '=':
+                yield self._match_or_else('=', 'eq', 'unknown')
             else:
-                raise LexerError(lexer_position=scanner.pos,
-                                 lexer_value=scanner.current,
-                                 message="Unknown token %s" % scanner.current)
+                raise LexerError(lexer_position=self._pos,
+                                 lexer_value=self._current,
+                                 message="Unknown token %s" % self._current)
         yield {'type': 'eof', 'value': '',
-               'start': len(expression), 'end': len(expression)}
+               'start': self._len, 'end': self._len}
 
-    def _consume_literal(self, scanner):
-        start = scanner.pos
-        lexeme = scanner.in_delimiter('`')
+    def _init_expr(self, expression):
+        if not expression:
+            raise EmptyExpressionError()
+        self._pos = 0
+        self._expression = expression
+        self._chars = list(self._expression)
+        self._current = self._chars[self._pos]
+        self._len = len(self._expression)
+
+    def _next(self):
+        if self._pos == self._len - 1:
+            self._current = None
+        else:
+            self._pos += 1
+            self._current = self._chars[self._pos]
+        return self._current
+
+    def _in_delimiter(self, delimiter):
+        start = self._pos
+        buff = ''
+        self._next()
+        while self._current != delimiter:
+            if self._current == '\\':
+                buff += '\\'
+                self._next()
+            if self._current is None:
+                raise LexerError(lexer_position=start,
+                                 lexer_value=self._expression,
+                                 message="Unclosed %s delimiter" % delimiter)
+            buff += self._current
+            self._next()
+        # Skip the closing delimiter.
+        self._next()
+        return buff
+
+    def _consume_literal(self):
+        start = self._pos
+        lexeme = self._in_delimiter('`')
         lexeme = lexeme.replace('\\`', '`')
         try:
             # Assume it is valid JSON and attempt to parse.
@@ -139,17 +137,17 @@ class Lexer(object):
                               PendingDeprecationWarning)
             except ValueError:
                 raise LexerError(lexer_position=start,
-                                 lexer_value=lexeme,
+                                 lexer_value=self._expression,
                                  message="Bad token %s" % lexeme)
-        token_len = scanner.pos - start
+        token_len = self._pos - start
         return {'type': 'literal', 'value': parsed_json,
                 'start': start, 'end': token_len}
 
-    def _consume_quoted_identifier(self, scanner):
-        start = scanner.pos
-        lexeme = '"' + scanner.in_delimiter('"') + '"'
+    def _consume_quoted_identifier(self):
+        start = self._pos
+        lexeme = '"' + self._in_delimiter('"') + '"'
         try:
-            token_len = scanner.pos - start
+            token_len = self._pos - start
             return {'type': 'quoted_identifier', 'value': loads(lexeme),
                     'start': start, 'end': token_len}
         except ValueError as e:
@@ -158,19 +156,19 @@ class Lexer(object):
                              lexer_value=lexeme,
                              message=error_message)
 
-    def _consume_raw_string_literal(self, scanner):
-        start = scanner.pos
-        lexeme = scanner.in_delimiter("'")
-        token_len = scanner.pos - start
+    def _consume_raw_string_literal(self):
+        start = self._pos
+        lexeme = self._in_delimiter("'")
+        token_len = self._pos - start
         return {'type': 'literal', 'value': lexeme,
                 'start': start, 'end': token_len}
 
-    def _match_or_else(self, scanner, expected, match_type, else_type):
-        start = scanner.pos
-        current = scanner.current
-        next_char = scanner.next()
+    def _match_or_else(self, expected, match_type, else_type):
+        start = self._pos
+        current = self._current
+        next_char = self._next()
         if next_char == expected:
-            scanner.next()
+            self._next()
             return {'type': match_type, 'value': current + next_char,
                     'start': start, 'end': start + 1}
         return {'type': else_type, 'value': current,
