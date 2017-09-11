803c0859502becad044d22ae71e9785315df8410
Giuliano Mega <mega@spaziodati.eu>
2017-09-10 21:24:40 -0300

    bugfix: reverse type of null should be NoneType, not None

---
+++ tests/test_custom_functions.py
@@ -0,0 +1,25 @@
+import unittest
+
+import functions
+import jmespath
+
+
+class CustomFunctions(functions.Functions):
+    @functions.signature({'types': ['string', 'array', 'object', 'null']})
+    def _func_length0(self, s):
+        return 0 if s is None else len(s)
+
+
+class TestCustomFunctions(unittest.TestCase):
+    def setUp(self):
+        self.options = jmespath.Options(custom_functions=CustomFunctions())
+
+    def test_allows_null(self):
+        data = {
+            'a': {
+                'b': [1, 2, 3]
+            }
+        }
+
+        self.assertEqual(jmespath.search('length0(a.b)', data, self.options), 3)
+        self.assertEqual(jmespath.search('length0(a.c)', data, self.options), 0)
