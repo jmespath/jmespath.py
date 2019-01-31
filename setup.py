4a3e428b51140448fa23b70351f1db7081219f24
Hugo <hugovk@users.noreply.github.com>
2019-01-31 23:04:42 +0200

    Add support for Python 3.7

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import os
-import sys
 import io
 
 from setuptools import setup, find_packages
@@ -32,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
