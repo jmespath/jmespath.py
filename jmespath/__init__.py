336310d8ef64217aa03e985187c28112064c2b82
Hugo <hugovk@users.noreply.github.com>
2019-01-31 22:48:33 +0200

    Warn if using an EOL Python

--- jmespath/__init__.py
+++ jmespath/__init__.py
@@ -1,7 +1,17 @@
+import warnings
+import sys
 from jmespath import parser
 from jmespath.visitor import Options
 
 __version__ = '0.9.5'
+
+
+if sys.version_info[:2] <= (2, 6) or ((3, 0) <= sys.version_info[:2] <= (3, 3)):
+    python_ver = '.'.join(str(x) for x in sys.version_info[:3])
+
+    warnings.warn('You are using Python {0}, which will no longer be supported in '
+                  'JMESPath 1.0'.format(python_ver),
+                  DeprecationWarning)
 
 
 def compile(expression):
