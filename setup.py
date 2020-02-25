249fb210328b7f7afe5125e7e1ee6e40aeaf30aa
James Saryerwinnie <js@jamesls.com>
2020-02-25 17:05:43 -0500

    Update version where py26/py33 will be dropped

--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ from setuptools import setup, find_packages
 if sys.version_info[:2] <= (2, 6) or ((3, 0) <= sys.version_info[:2] <= (3, 3)):
     python_ver = '.'.join(str(x) for x in sys.version_info[:3])
 
-    warnings.warn('You are using Python {0}, which will no longer be supported in '
-                  'JMESPath 1.0'.format(python_ver),
-                  PendingDeprecationWarning)
+    warnings.warn(
+        'You are using Python {0}, which will no longer be supported in '
+        'version 0.11.0'.format(python_ver),
+        DeprecationWarning)
 
 
 setup(
