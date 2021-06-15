jmespath/jmespath.py

commit 0bbc90f4cfe219339c8b2ecfccff5932565334fd

jpp: Add --accumulate, -a option which accumulates all output objects into a single recursively merged object
This option will accumulate all output objects into a single
recursively merged output object.

Reference golang implementation:

https://github.com/pipebus/jpp/commit/e7989df1056486dbf3e1c3b53a2f208a2023efc9

lib/jpipe/jpp/main.py
@@ -3,6 +3,7 @@ import json
 import os
 import pprint
 import sys
+import itertools
 
 import jmespath
 
@@ -65,6 +66,16 @@ def jpp_main(argv=None):
         prog=os.path.basename(argv[0]),
     )
     parser.add_argument("expression", nargs="?", default=None)
+    parser.add_argument(
+        "-a",
+        "--accumulate",
+        action="store_true",
+        dest="accumulate",
+        default=False,
+        help=(
+            "Accumulate all output objects into a single recursively merged output object."
+        ),
+    )
     parser.add_argument(
         "-c",
         "--compact",
@@ -152,28 +163,77 @@ def jpp_main(argv=None):
     else:
         f = sys.stdin
 
+    accumulator = None
+    eof = False
+
     with f:
         stream_iter = decode_json_stream(f)
         while True:
-            if args.slurp:
-                data = list(stream_iter)
-                if not data:
-                    break
-            else:
-                try:
-                    data = next(stream_iter)
-                except StopIteration:
+            while True:
+                if args.slurp:
+                    data = list(stream_iter)
+                    if not data:
+                        eof = True
+                        break
+                else:
+                    try:
+                        data = next(stream_iter)
+                    except StopIteration:
+                        eof = True
+                        break
+
+                result = jmespath.search(expression, data)
+
+                if args.accumulate:
+                    if accumulator is None:
+                        accumulator = result
+                    else:
+                        accumulator = merge(accumulator, result)
+                else:
                     break
 
-            result = jmespath.search(expression, data)
+            if args.accumulate:
+                result = accumulator
+            elif eof:
+                break
+
             if args.quoted or not isinstance(result, str):
                 result = json.dumps(result, **dump_kwargs)
 
             sys.stdout.write(result)
             sys.stdout.write("\n")
-            if args.slurp:
+            if eof or args.accumulate or args.slurp:
                 break
     return 0
+
+
+def merge(base, head):
+    """
+    Recursively merge head onto base.
+    """
+    if isinstance(head, dict):
+        if not isinstance(base, dict):
+            return head
+
+        result = {}
+        for k in itertools.chain(head, base):
+            try:
+                result[k] = merge(base[k], head[k])
+            except KeyError:
+                try:
+                    result[k] = head[k]
+                except KeyError:
+                    result[k] = base[k]
+
+    elif isinstance(head, list):
+        result = []
+        if isinstance(base, list):
+            result.extend(base)
+        result.extend(head)
+    else:
+        result = head
+
+    return result
 
 
 if __name__ == "__main__":

test/test_jpp.py
@@ -21,6 +21,9 @@ class JPPTest(unittest.TestCase):
             ((), """{"hello": "world"}""", "@", '{\n  "hello": "world"\n}\n', 0),
             (("-c",), """{"hello": "world"}""", "@", '{"hello":"world"}\n', 0),
             (("-c",), """{\n  "foo": "bar"\n}{   \n"foo": "x"\n}""", "@", '{"foo":"bar"}\n{"foo":"x"}\n', 0),
+            (("-a", "-c",), """{"foo": ["a"]}{"foo": ["x"]}""", "@", '{"foo":["a","x"]}\n', 0),
+            (("-a", "-c",), """["a"]["x"]""", "@", '["a","x"]\n', 0),
+            (("-c", "-s",), """"a"\n"x"\n""", "@", '["a","x"]\n', 0),
             ((), """{"hello": "world"}""", "@.hello", '"world"\n', 0),
             (("-u",), """{"hello": "world"}""", "@.hello", "world\n", 0),
         ):

commit 72e273e216b79ab5d1a079c7f130034e95baf59a

jpp: fix --accumulate array merge to coalesce duplicates
Reference golang implementation:

https://github.com/pipebus/jpp/commit/0abe226af789977087407929fba0e4cc85f9c5a1

Fixes: 0bbc90f4cfe2 ("jpp: Add --accumulate, -a option which accumulates all output objects into a single recursively merged object")

lib/jpipe/jpp/main.py
@@ -229,7 +229,11 @@ def merge(base, head):
         result = []
         if isinstance(base, list):
             result.extend(base)
-        result.extend(head)
+            for node in head:
+                if node not in result:
+                    result.append(node)
+        else:
+            result.extend(head)
     else:
         result = head
 

setup.py
@@ -8,7 +8,7 @@ from setuptools import (
     setup,
 )
 
-__version__ = "0.1.3.1"
+__version__ = "0.1.3.2"
 
 sys.path.insert(0, "lib")
 from jpipe import (

test/test_jpp.py
@@ -21,7 +21,10 @@ class JPPTest(unittest.TestCase):
             ((), """{"hello": "world"}""", "@", '{\n  "hello": "world"\n}\n', 0),
             (("-c",), """{"hello": "world"}""", "@", '{"hello":"world"}\n', 0),
             (("-c",), """{\n  "foo": "bar"\n}{   \n"foo": "x"\n}""", "@", '{"foo":"bar"}\n{"foo":"x"}\n', 0),
-            (("-a", "-c",), """{"foo": ["a"]}{"foo": ["x"]}""", "@", '{"foo":["a","x"]}\n', 0),
+            (("-a", "-c",), """{"foo": ["a"]}{"foo": ["a", "x"]}""", "@", '{"foo":["a","x"]}\n', 0),
+            (("-a", "-c",), """{"foo": ["a"]}{"foo": ["a"]}""", "@", '{"foo":["a"]}\n', 0),
+            (("-a", "-c",), """{"foo": ["a", "a"]}{"foo": ["a"]}""", "@", '{"foo":["a","a"]}\n', 0),
+            (("-a", "-c",), """{"foo": ["a", "a"]}{"foo": ["a", "b"]}""", "@", '{"foo":["a","a","b"]}\n', 0),
             (("-a", "-c",), """["a"]["x"]""", "@", '["a","x"]\n', 0),
             (("-c", "-s",), """"a"\n"x"\n""", "@", '["a","x"]\n', 0),
             ((), """{"hello": "world"}""", "@.hello", '"world"\n', 0),