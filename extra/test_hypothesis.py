f71ca20598d1cd315d0fff1e8dd6ce5310e8ed10
Nate Prewitt <nate.prewitt@gmail.com>
2022-03-15 17:29:13 -0600

    Remove remaining Python 2.6 cruft

--- extra/test_hypothesis.py
+++ extra/test_hypothesis.py
@@ -3,7 +3,6 @@
 # can a) be run separately and b) allow for customization
 # via env var for longer runs in travis.
 import os
-import sys
 import numbers
 
 from hypothesis import given, settings, assume, HealthCheck
@@ -13,11 +12,6 @@ from jmespath import lexer
 from jmespath import parser
 from jmespath import exceptions
 from jmespath.functions import Functions
-
-
-if sys.version_info[:2] == (2, 6):
-    raise RuntimeError("Hypothesis tests are not supported on python2.6. "
-                       "Use python2.7, or python3.3 and greater.")
 
 
 JSON_NUMBERS = (st.integers() | st.floats(allow_nan=False,
